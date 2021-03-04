7b5823b1d271c27e69f8ad62a63f7efa24c1a495
jadamcrain <jadamcrain@gmail.com>
2021-03-04 10:59:33 -0800

    map UTF8Error

--- tests/foo-ffi/src/error.rs
+++ tests/foo-ffi/src/error.rs
@@ -1,9 +1,16 @@
 use std::ffi::CStr;
+use std::str::Utf8Error;
 
 pub(crate) fn get_special_number(password: &CStr) -> std::result::Result<u32, crate::ffi::MyError> {
-    if password.to_str().unwrap() == "solarwinds123" {
+    if password.to_str()? == "solarwinds123" {
         Ok(42)
     } else {
         Err(crate::ffi::MyError::BadPassword)
+    }
+}
+
+impl From<Utf8Error> for crate::ffi::MyError {
+    fn from(_: Utf8Error) -> Self {
+        crate::ffi::MyError::BadPassword
     }
 }

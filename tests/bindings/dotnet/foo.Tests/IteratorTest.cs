8ca5d5baba7f0a7593850954b29d9b10d6b63d55
jadamcrain <jadamcrain@gmail.com>
2021-10-20 08:25:30 -0700

    Add chunk iterator tests to C#

--- tests/bindings/dotnet/foo.Tests/IteratorTest.cs
+++ tests/bindings/dotnet/foo.Tests/IteratorTest.cs
@@ -21,6 +21,23 @@ namespace foo.Tests
             }
         }
 
+        class ChunkReceiver : IChunkReceiver
+        {
+            public readonly List<List<Char>> values = new List<List<Char>>();
+
+            public void OnChunk(ICollection<Chunk> values)
+            {
+                foreach(Chunk c in values)
+                {
+                    List<Char> bytes = new List<Char>();
+                    foreach(ByteValue bv in c.Iter) {
+                        bytes.Add(Convert.ToChar(bv.Value));
+                    }
+                    this.values.Add(bytes);
+                }
+            }
+        }
+
         [Fact]
         public void StringIteratorTest()
         {
@@ -29,7 +46,19 @@ namespace foo.Tests
             IteratorTestHelper.InvokeCallback("ABCDE", receiver);
             Assert.Equal(new byte[] { 65, 66, 67, 68, 69 }, receiver.values);
         }
-        
+
+        [Fact]
+        public void ChunkIteratorTest()
+        {
+            var receiver = new ChunkReceiver();
+            DoubleIteratorTestHelper.IterateStringByChunks("Hello World!", 3, receiver);
+            Assert.Equal(4, receiver.values.Count);
+            Assert.Equal(receiver.values[0], new char[] { 'H', 'e', 'l' });
+            Assert.Equal(receiver.values[1], new char[] { 'l', 'o', ' ' });
+            Assert.Equal(receiver.values[2], new char[] { 'W', 'o', 'r' });
+            Assert.Equal(receiver.values[3], new char[] { 'l', 'd', '!' });
+        }
+
     }
 }
 

a2c80f86d6e5723a1f76b9c09b83ecdf65f662a1
jadamcrain <jadamcrain@gmail.com>
2021-11-19 16:18:45 -0800

    generate wrapper functions around Java native functions that perform the null checks

--- generators/java-oo-bindgen/src/java/conversion.rs
+++ generators/java-oo-bindgen/src/java/conversion.rs
@@ -8,6 +8,16 @@ pub(crate) trait JavaType {
     fn as_java_object(&self) -> String;
 }
 
+/// Describes if the type is a primitive in Java
+pub(crate) trait IsPrimitive {
+    fn is_primitive(&self) -> bool;
+
+    /// Anything that isn't a primitive can be a null reference
+    fn is_nullable(&self) -> bool {
+        !self.is_primitive()
+    }
+}
+
 impl JavaType for Primitive {
     fn as_java_primitive(&self) -> String {
         match self {

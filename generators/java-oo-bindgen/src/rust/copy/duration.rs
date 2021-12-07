218d4ae0b7ae5bf056a47e85a056faf0dfb11d8b
jadamcrain <jadamcrain@gmail.com>
2021-12-07 14:42:40 -0800

    simplify duration cache functions

--- generators/java-oo-bindgen/src/rust/copy/duration.rs
+++ generators/java-oo-bindgen/src/rust/copy/duration.rs
@@ -33,44 +33,23 @@ impl Duration {
         }
     }
 
-    pub fn duration_to_millis(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> u64 {
+    pub fn to_millis(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> u64 {
         env.call_method_unchecked(obj, self.to_millis_method, JavaType::Primitive(Primitive::Long), &[]).unwrap()
             .j().unwrap() as u64
     }
 
-    pub fn duration_from_millis(&self, env: &jni::JNIEnv, millis: u64) -> jni::sys::jobject {
+    pub fn from_millis(&self, env: &jni::JNIEnv, millis: u64) -> jni::sys::jobject {
         env.call_static_method_unchecked(&self.class, self.of_millis_method, JavaType::Object("java/time/Duration".to_string()), &[JValue::Long(millis as i64)]).unwrap()
             .l().unwrap().into_inner()
     }
 
-    pub fn duration_to_seconds(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> u64 {
+    pub fn to_seconds(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> u64 {
         env.call_method_unchecked(obj, self.get_seconds_method, JavaType::Primitive(Primitive::Long), &[]).unwrap()
             .j().unwrap() as u64
     }
 
-    pub fn duration_from_seconds(&self, env: &jni::JNIEnv, seconds: u64) -> jni::sys::jobject {
+    pub fn from_seconds(&self, env: &jni::JNIEnv, seconds: u64) -> jni::sys::jobject {
         env.call_static_method_unchecked(&self.class, self.of_seconds_method, JavaType::Object("java/time/Duration".to_string()), &[JValue::Long(seconds as i64)]).unwrap()
-            .l().unwrap().into_inner()
-    }
-
-    pub fn duration_to_seconds_float(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> f32 {
-        let seconds = env.call_method_unchecked(obj, self.get_seconds_method, JavaType::Primitive(Primitive::Long), &[]).unwrap()
-            .j().unwrap() as u64;
-
-        let nanos = env.call_method_unchecked(obj, self.get_nano_method, JavaType::Primitive(Primitive::Int), &[]).unwrap()
-            .i().unwrap() as u64;
-
-        (seconds as f32) + ((nanos as f32) / 1_000_000_000.0)
-    }
-
-    pub fn duration_from_seconds_float(&self, env: &jni::JNIEnv, value: f32) -> jni::sys::jobject {
-        let seconds = value as i64;
-        let nano = ((value - value.trunc()) * 1_000_000_000.0) as i32;
-
-        let obj = env.call_static_method_unchecked(&self.class, self.of_seconds_method, JavaType::Object("java/time/Duration".to_string()), &[JValue::Long(seconds)]).unwrap()
-            .l().unwrap();
-
-        env.call_method_unchecked(obj, self.with_nanos_method, JavaType::Object("java/time/Duration".to_string()), &[JValue::Int(nano)]).unwrap()
             .l().unwrap().into_inner()
     }
 }

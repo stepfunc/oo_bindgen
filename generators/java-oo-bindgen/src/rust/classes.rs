214d53111276cc1a6947ad473adf3a6cb51c2da7
jadamcrain <jadamcrain@gmail.com>
2021-12-10 10:39:06 -0800

    initial refactoring of class cache

--- generators/java-oo-bindgen/src/rust/classes.rs
+++ generators/java-oo-bindgen/src/rust/classes.rs
@@ -13,22 +13,53 @@ pub(crate) fn generate_classes_cache(
     filename.set_extension("rs");
     let mut f = FilePrinter::new(&filename)?;
 
-    f.writeln("pub(crate) struct Classes")?;
-    blocked(&mut f, |f| {
+    f.writeln("pub(crate) struct ClassInfo {")?;
+    indented(&mut f, |f| {
+        f.writeln("class: jni::objects::GlobalRef,")?;
+        f.writeln("constructor: jni::objects::JMethodID<'static>,")?;
+        f.writeln("self_field: jni::objects::JFieldID<'static>,")
+    })?;
+    f.writeln("}")?;
+
+    f.newline()?;
+
+    f.writeln("impl ClassInfo {")?;
+    indented(&mut f, |f| {
         for class in lib.classes() {
-            f.writeln(&format!("class_{}: jni::objects::GlobalRef,", class.name()))?;
+            f.newline()?;
             f.writeln(&format!(
-                "{}_constructor: jni::objects::JMethodID<'static>,",
+                "fn init_{}(env: &jni::JNIEnv) -> Self {{",
                 class.name()
             ))?;
-            f.writeln(&format!(
-                "{}_self_field: jni::objects::JFieldID<'static>,",
-                class.name()
-            ))?;
+            indented(f, |f| {
+                let class_name = class.name().camel_case();
+                f.writeln(&format!(
+                    "let class = env.find_class(\"L{}/{};\").expect(\"Unable to find class {}\");",
+                    lib_path, class_name, class_name
+                ))?;
+                f.writeln(&format!("let constructor = env.get_method_id(class, \"<init>\", \"(J)V\").map(|mid| mid.into_inner().into()).expect(\"Unable to find constructor of {}\");", class_name))?;
+                f.writeln(&format!("let self_field = env.get_field_id(class, \"self\", \"J\").map(|mid| mid.into_inner().into()).expect(\"Unable to find self field of {}\");", class_name))?;
+                f.writeln(
+                    "Self { class: env.new_global_ref(class).unwrap(), constructor, self_field }",
+                )
+            })?;
+            f.writeln("}")?;
+        }
+        Ok(())
+    })?;
+    f.writeln("}")?;
+
+    f.newline()?;
+
+    f.writeln("pub(crate) struct Classes {")?;
+    indented(&mut f, |f| {
+        for class in lib.classes() {
+            f.writeln(&format!("{}: ClassInfo,", class.name()))?;
         }
 
         Ok(())
     })?;
+    f.writeln("}")?;
 
     f.newline()?;
 
@@ -36,24 +67,14 @@ pub(crate) fn generate_classes_cache(
     blocked(&mut f, |f| {
         f.writeln("pub(crate) fn init(env: &jni::JNIEnv) -> Self")?;
         blocked(f, |f| {
-            for class in lib.classes() {
-                let class_name = class.name().camel_case();
-                let snake_name = class.name();
-                f.writeln(&format!("let class_{snake_name} = env.find_class(\"L{lib_path}/{class_name};\").expect(\"Unable to find class {class_name}\");", snake_name=snake_name, class_name=class_name, lib_path=lib_path))?;
-                f.writeln(&format!("let {snake_name}_constructor = env.get_method_id(class_{snake_name}, \"<init>\", \"(J)V\").map(|mid| mid.into_inner().into()).expect(\"Unable to find constructor of {class_name}\");", snake_name=snake_name, class_name=class_name))?;
-                f.writeln(&format!("let {snake_name}_self_field = env.get_field_id(class_{snake_name}, \"self\", \"J\").map(|mid| mid.into_inner().into()).expect(\"Unable to find self field of {class_name}\");", snake_name=snake_name, class_name=class_name))?;
-            }
-
             f.writeln("Self")?;
             blocked(f, |f| {
                 for class in lib.classes() {
-                    let snake_name = class.name();
                     f.writeln(&format!(
-                        "class_{}: env.new_global_ref(class_{}).unwrap(),",
-                        snake_name, snake_name
+                        "{}: ClassInfo::init_{}(env),",
+                        class.name(),
+                        class.name()
                     ))?;
-                    f.writeln(&format!("{}_constructor,", snake_name))?;
-                    f.writeln(&format!("{}_self_field,", snake_name))?;
                 }
                 Ok(())
             })
@@ -63,18 +84,17 @@ pub(crate) fn generate_classes_cache(
 
         for class in lib.classes() {
             let class_name = class.name().camel_case();
-            let snake_name = class.name();
 
-            f.writeln(&format!("pub(crate) fn {snake_name}_to_rust(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> *mut {ffi_name}::{class_name}", snake_name=snake_name, ffi_name=config.ffi_name, class_name=class_name))?;
+            f.writeln(&format!("pub(crate) fn {}_to_rust(&self, env: &jni::JNIEnv, obj: jni::sys::jobject) -> *mut {}::{}", class.name(), config.ffi_name, class_name))?;
             blocked(f, |f| {
-                f.writeln(&format!("env.get_field_unchecked(obj, self.{snake_name}_self_field, jni::signature::JavaType::Primitive(jni::signature::Primitive::Long)).unwrap().j().unwrap() as *mut _", snake_name=snake_name))
+                f.writeln(&format!("env.get_field_unchecked(obj, self.{}.self_field, jni::signature::JavaType::Primitive(jni::signature::Primitive::Long)).unwrap().j().unwrap() as *mut _", class.name()))
             })?;
 
             f.newline()?;
 
-            f.writeln(&format!("pub(crate) fn {snake_name}_from_rust(&self, env: &jni::JNIEnv, value: *mut {ffi_name}::{class_name}) -> jni::sys::jobject", snake_name=snake_name, ffi_name=config.ffi_name, class_name=class_name))?;
+            f.writeln(&format!("pub(crate) fn {}_from_rust(&self, env: &jni::JNIEnv, value: *mut {}::{}) -> jni::sys::jobject", class.name(), config.ffi_name, class_name))?;
             blocked(f, |f| {
-                f.writeln(&format!("env.new_object_unchecked(&self.class_{snake_name}, self.{snake_name}_constructor, &[jni::objects::JValue::Long(value as i64)]).unwrap().into_inner()", snake_name=snake_name))
+                f.writeln(&format!("env.new_object_unchecked(&self.{}.class, self.{}.constructor, &[jni::objects::JValue::Long(value as i64)]).unwrap().into_inner()", class.name(), class.name()))
             })?;
         }
 

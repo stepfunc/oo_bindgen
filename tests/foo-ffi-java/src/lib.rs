b96e23548d2b4cbcf47ac2f394238a3fb04aa2fb
jadamcrain <jadamcrain@gmail.com>
2022-05-11 22:52:55 -0700

    allow certain things in the generated jni

--- tests/foo-ffi-java/src/lib.rs
+++ tests/foo-ffi-java/src/lib.rs
@@ -1,2 +1,11 @@
+#![allow(
+    clippy::unused_unit,
+    clippy::useless_conversion,
+    clippy::redundant_closure,
+    clippy::needless_borrow,
+    clippy::needless_return,
+    clippy::not_unsafe_ptr_arg_deref
+)]
+
 mod generated;
 pub use generated::*;
